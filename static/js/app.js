54ea4ab72808fd8eae9e63a6cbee83c98b3e0598
PDob02 <patrickdobry@gmail.com>
2022-03-02 19:02:03 -0600

    Before Wednesday Class

--- static/js/app.js
+++ static/js/app.js
@@ -8,3 +8,95 @@ var tbody = d3.select("tbody");
 function printHello() {
     console.log("Hello there!");
   }
+
+// Takes two numbers and adds them
+function addition(a, b) {
+    return a + b;
+  }
+  console.log(addition(4, 5));
+
+  // Functions can call other functions
+function doubleAddition(c, d) {
+    var total = addition(c, d) * 2;
+    return total;
+  }
+
+  // Simple JavaScript log statement
+function printHello() {
+    return "Hello there!";
+  }
+
+  printHello = () => "Hello there!";
+
+  // Original addition function
+function addition(a, b) {
+    return a + b;
+  }
+// Converted to an arrow function
+addition = (a, b) => a + b;
+
+// Original doubleAddition function
+function doubleAddition(c, d) {
+    var total = addition(c, d) * 2;
+    return total;
+  }
+
+  doubleAddition = (c, d)
+  => addition(c, d) * 2;
+
+  let friends = ["Sarah", "Greg", "Cindy", "Jeff"];
+
+  function listLoop(userList) {
+    for (var i = 0; i < userList.length; i++) {
+      console.log(userList[i]);
+    }
+ }
+//  let vegetables = ["Carrots", "Peas", "Lettuce", "Tomatoes"];
+
+//  for (var i = 0; i < vegetables.length; i++) {
+// }
+ var vegetables = ["Carrots", "Peas", "Lettuce", "Tomatoes"];
+
+ for (var i = 0; i < vegetables.length; i++) {
+     console.log("I love " + vegetables[i]);
+ }
+ 
+//  function buildTable(data) {
+//     tbody.html("");
+//   }
+function buildTable(data) {
+    tbody.html("");
+  }
+
+data.forEach((dataRow) => {
+    let row = tbody.append("tr");
+});
+
+data.forEach((dataRow) => {
+    let row = tbody.append("tr");
+    Object.values(dataRow).forEach((val) => {
+      let cell = row.append("td");
+      cell.text(val);
+      }
+    );
+  });
+
+  function buildTable(data) {
+    // First, clear out any existing data
+    tbody.html("");
+  
+    // Next, loop through each object in the data
+    // and append a row and cells for each value in the row
+    data.forEach((dataRow) => {
+      // Append a row to the table body
+      let row = tbody.append("tr");
+  
+      // Loop through each field in the dataRow and add
+      // each value as a table cell (td)
+      Object.values(dataRow).forEach((val) => {
+        let cell = row.append("td");
+        cell.text(val);
+        }
+      );
+    });
+  }
