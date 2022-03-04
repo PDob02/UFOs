29aca96740ef06080f5d5fdf33326c8ef6537b8c
PDob02 <patrickdobry@gmail.com>
2022-03-04 09:02:47 -0600

    Speaking with Ask BCS

--- static/js/app.js
+++ static/js/app.js
@@ -4,82 +4,82 @@ const tableData = data;
 // Reference the HTML table using d3
 var tbody = d3.select("tbody");
 
-// Simple JavaScript console.log statement
-function printHello() {
-    console.log("Hello there!");
-  }
+// // Simple JavaScript console.log statement
+// function printHello() {
+//     console.log("Hello there!");
+//   }
 
-// Takes two numbers and adds them
-function addition(a, b) {
-    return a + b;
-  }
-  console.log(addition(4, 5));
+// // Takes two numbers and adds them
+// function addition(a, b) {
+//     return a + b;
+//   }
+//   console.log(addition(4, 5));
 
-  // Functions can call other functions
-function doubleAddition(c, d) {
-    var total = addition(c, d) * 2;
-    return total;
-  }
+//   // Functions can call other functions
+// function doubleAddition(c, d) {
+//     var total = addition(c, d) * 2;
+//     return total;
+//   }
 
-  // Simple JavaScript log statement
-function printHello() {
-    return "Hello there!";
-  }
+//   // Simple JavaScript log statement
+// function printHello() {
+//     return "Hello there!";
+//   }
 
-  printHello = () => "Hello there!";
+//   printHello = () => "Hello there!";
 
-  // Original addition function
-function addition(a, b) {
-    return a + b;
-  }
-// Converted to an arrow function
-addition = (a, b) => a + b;
+//   // Original addition function
+// function addition(a, b) {
+//     return a + b;
+//   }
+// // Converted to an arrow function
+// addition = (a, b) => a + b;
 
-// Original doubleAddition function
-function doubleAddition(c, d) {
-    var total = addition(c, d) * 2;
-    return total;
-  }
+// // Original doubleAddition function
+// function doubleAddition(c, d) {
+//     var total = addition(c, d) * 2;
+//     return total;
+//   }
 
-  doubleAddition = (c, d)
-  => addition(c, d) * 2;
+//   doubleAddition = (c, d)
+//   => addition(c, d) * 2;
 
-  let friends = ["Sarah", "Greg", "Cindy", "Jeff"];
+//   let friends = ["Sarah", "Greg", "Cindy", "Jeff"];
 
-  function listLoop(userList) {
-    for (var i = 0; i < userList.length; i++) {
-      console.log(userList[i]);
-    }
- }
+//   function listLoop(userList) {
+//     for (var i = 0; i < userList.length; i++) {
+//       console.log(userList[i]);
+//     }
+//  }
 //  let vegetables = ["Carrots", "Peas", "Lettuce", "Tomatoes"];
 
 //  for (var i = 0; i < vegetables.length; i++) {
 // }
- var vegetables = ["Carrots", "Peas", "Lettuce", "Tomatoes"];
+//  var vegetables = ["Carrots", "Peas", "Lettuce", "Tomatoes"];
 
- for (var i = 0; i < vegetables.length; i++) {
-     console.log("I love " + vegetables[i]);
- }
+//  for (var i = 0; i < vegetables.length; i++) {
+//      console.log("I love " + vegetables[i]);
+//  }
  
 //  function buildTable(data) {
 //     tbody.html("");
 //   }
-function buildTable(data) {
-    tbody.html("");
-  }
+// function buildTable(data) {
+//     tbody.html("");
+//   }
 
-data.forEach((dataRow) => {
-    let row = tbody.append("tr");
-});
+// data.forEach((dataRow) => {
+//     let row = tbody.append("tr");
+// });
 
-data.forEach((dataRow) => {
-    let row = tbody.append("tr");
-    Object.values(dataRow).forEach((val) => {
-      let cell = row.append("td");
-      cell.text(val);
-      }
-    );
-  });
+// data.forEach((dataRow) => {
+//     let row = tbody.append("tr");
+//     Object.values(dataRow).forEach((val) => {
+//       let cell = row.append("td");
+//       cell.text(val);
+//       }
+//     );
+//   });
 
   function buildTable(data) {
     // First, clear out any existing data
